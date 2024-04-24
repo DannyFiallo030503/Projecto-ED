c8720923dfcb12653bbdd8f29e423b81b5619aa5
DanFin <dan.fiallo.03@gmail.com>
2024-04-23 20:25:52 -0400

    new

--- Resuourse/Map/Box.java
+++ Resuourse/Map/Box.java
@@ -2,24 +2,11 @@ package Resuourse.Map;
 
 public abstract class Box {
 
-    protected int x;
-    protected int y;
-
-    public float CalcDist(int posX, int posY) {
+    public float CalcDist(int posX, int posY, int x, int y) {
         float dist = 0;
 
         dist = (float) Math.sqrt(Math.pow((posX - x), 2) + Math.pow((posY - y), 2));
 
         return dist;
     }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-
 }
